4f52815448f4387adf83190ba887d30f6074c34e
tjosm <33854926+tjosm@users.noreply.github.com>
2018-08-15 20:14:42 +0530

    update database info

--- 202-config.php
+++ 202-config.php
@@ -1,9 +1,9 @@
 <?php
 
 // ** MySQL settings ** //
-$dbname = 'prosper202';    		// The name of the database
-$dbuser = 'freddy';     		  		// Your MySQL username
-$dbpass = 'dnPuAVzNQiw1JePO'; 			// ...and password
+$dbname = 'database';    		// The name of the database
+$dbuser = 'user';     		  		// Your MySQL username
+$dbpass = 'password'; 			// ...and password
 $dbhost = 'localhost';    					// 99% chance you won't need to change this value
 $mchost = 'localhost';  					//this is the memcache server host, if you don't know what this is, don't touch it.
 
